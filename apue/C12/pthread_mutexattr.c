fb35fb357289aca94aea1c578278d8665b10109d
daijinwei <daijinwei41@gmail.com>
2015-04-09 20:47:01 +0800

    modify mutexattr

--- apue/C12/pthread_mutexattr.c
+++ apue/C12/pthread_mutexattr.c
@@ -10,10 +10,21 @@
 #include <pthread.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 pthread_mutex_t *f_lock = NULL;
 
-int count = 0;
+struct shared_context{
+    int ref_cnt;      // counter
+    int last_process;
+};
+
+struct shared_context * sctx = NULL;
+//= (struct shared_context*)shmat(shmid, 0, 0);
+
+#define key_shared_context 1234
+
 
 int main()
 {
@@ -38,22 +49,37 @@ int main()
         exit(1);
     }
 
-    pthread_mutex_init(f_lock, &attr);
+    ret = pthread_mutex_init(f_lock, &attr);
+    if(0 != ret){
+        printf("init mutexattr  shared failed\n");
+        exit(1);
+    }
 
-    pid_t pid;
-    pid = fork();
-    if(pid < 0){
+    key_t shmid = shmget(key_shared_context, sizeof(struct shared_context), IPC_CREAT | 0660);
+    struct shared_context *stex = (struct shared_context*)shmat(shmid, 0, 0);
+
+    pid_t pid = fork();
+    if(pid <  0){
         perror("fork faild");
     }else if(pid > 0){
+        printf("parent\n");
         pthread_mutex_lock(f_lock);
-        count++;
-        printf("count\t%d\n", count);
+        sctx->ref_cnt++;
+        sctx->last_process = getpid();
+        shmdt((char*)sctx);
         pthread_mutex_unlock(f_lock);
     }else{
+        printf("chiled\n");
         pthread_mutex_lock(f_lock);
-        count++;
-        printf("count\t%d\n", count);
+        //struct shared_context * 
+        sctx = (struct shared_context*)shmat(shmid, 0, 0);
+        sctx->ref_cnt++;
+        sctx->last_process = getpid();
+        shmdt((char*)sctx);
         pthread_mutex_unlock(f_lock);
     }
+    struct shmid_ds buf[1234];
+    shmctl(shmid, IPC_RMID, buf);
+    return 0;
 
 }
