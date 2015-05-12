1660e6bde124cba7c5594203bca739c2b00b9fed
daijinwei <daijinwei41@gmail.com>
2015-05-12 15:40:01 +0800

    add io.c, fgetc

---
+++ apue/C05/io.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: io.c
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 12 May 2015 03:36:56 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+    while((n = fgetc(stdin))){
+        if(putc(n, stdout) == EOF){
+            printf("putc error\n");
+        }
+    }
+
+    if(ferror(stdin)){
+        printf("stdinerror\n");
+    }
+    exit(0);
+}
