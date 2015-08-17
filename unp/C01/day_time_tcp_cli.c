58319df8b24e96354d971904ada0e18f0a1db721
daijinwei <daijinwei41@gmail.com>
2015-08-17 07:11:52 -0700

    Modify day_time_tcp_cli

--- unp/C01/day_time_tcp_cli.c
+++ unp/C01/day_time_tcp_cli.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main()
+#define MAXLEN 1024
+
+int main(int argc, char* argv[])
 {
+	int sockfd, n;
+	char recvline[MAXLEN];
+	struct sockaddr_in server_addr;
+
+	if (2 != argc){
+		printf("Usage: ./day_time_tcp_cli <IPaddress>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		printf("sock failed\n");
+		exit(EXIT_FAILURE);
+	}
+
 	return 0;
 }
