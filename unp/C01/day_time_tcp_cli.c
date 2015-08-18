4be2b16689a04a8935be6ffa525d78e98c63d489
daijinwei <daijinwei41@gmail.com>
2015-08-18 06:32:03 -0700

    Implement of day_time_tcp_cli

--- unp/C01/day_time_tcp_cli.c
+++ unp/C01/day_time_tcp_cli.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,6 +9,7 @@
 
 int main(int argc, char* argv[])
 {
+	int ret;
 	int sockfd, n;
 	char recvline[MAXLEN];
 	struct sockaddr_in server_addr;
@@ -19,6 +21,32 @@ int main(int argc, char* argv[])
 
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		printf("sock failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(13);		// Day time server
+
+	if((inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0)) {
+		printf("inet_pton failed\n");	
+		exit(EXIT_FAILURE);
+	}
+
+	if ((ret = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0){
+		printf("connect to server failed\n");
+		exit(EXIT_FAILURE);
+	}	
+
+	while( (n = read(sockfd, recvline, MAXLEN)) > 0){
+		recvline[n] = '\0';
+		if(fputs(recvline, stdout) == EOF){
+			printf("connect to server failed\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	if(n < 0){
 		exit(EXIT_FAILURE);
 	}
 
