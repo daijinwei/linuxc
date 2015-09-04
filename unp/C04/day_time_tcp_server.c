ff92c1cc503a7fccd607499197335ffbd6d59c6e
daijinwei <daijinwei41@gmail.com>
2015-09-04 07:10:46 -0700

    Server care the client addr

---
+++ unp/C04/day_time_tcp_server.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include "log.h"
+
+#define MAX_LEN 1024
+#define BACK_LOG 1024
+
+int main(int argc, char* argv[])
+{
+	int ret;
+	int sockfd, connect_fd;
+        char buffer[MAX_LEN];
+        const char *message = NULL;
+	struct sockaddr_in server_addr, client_addr;
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+                LOG(ERROR, "create socket failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        server_addr.sin_port = htons(13);		// Day time server, port: 0-1024,Permission should be root
+
+	if ( -1 == (ret = bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)))){
+                message = strerror(errno);
+                LOG(ERROR, "Bind the sockaddr to socket failed, %s\n", message);
+		exit(EXIT_FAILURE);
+	}	
+
+        if (-1 == (ret = listen(sockfd, BACK_LOG))){
+                LOG(ERROR, "Seting listening socket failed\n");
+		exit(EXIT_FAILURE);
+        }
+
+        for ( ; ; ){
+                socklen_t len = sizeof(struct sockaddr);
+                if( -1 == (connect_fd = accept(sockfd, (struct sockaddr*) &client_addr, &len))){
+                        message = strerror(errno);
+                        LOG(ERROR, "Seting listening socket failed, %s\n", message);
+                        exit(EXIT_FAILURE);
+                }
+                printf("connection from %s: port %d\n", inet_ntop(AF_INET, &client_addr, buffer, sizeof(buffer)), ntohs(client_addr.sin_port));
+
+                time_t ticks = time(NULL);
+                snprintf(buffer, sizeof(buffer), "%.24s\r\n", ctime(&ticks));
+                if ((ret = write(connect_fd, buffer, strlen(buffer))) < 0){
+                        LOG(ERROR, "Seting listening socket failed, %s\n", message);
+                }
+                close(connect_fd);
+        }
+        close(sockfd);
+	return 0;
+}
---
+++ unp/C04/log.h
@@ -0,0 +1,35 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#define MAX_LOG_LEVEL   256
+
+#define BUF_SIZE        256
+#define BUF_TIME_SIZE   32
+
+// define log level
+#define NONE    0
+#define FATAL   1
+#define ERROR   2
+#define WARNING 3
+#define INFO    4
+#define DEBUG   5
+#define TRACE   6
+
+/**
+ * Get the now time, format year-month-day hours:min:seconds,
+ * for example, 2010-03-14 21:59:23.
+ * The format time stores the buf_time.
+ * @return: no return
+ */
+void get_now_time(char *buf_time);
+
+/**
+ * Write the message to the log file 
+ * Log format,  [level][time][file | line | pid ][message].
+ * For example, [INFO][2015-08-23 05:27:30][file: log.cpp | line: 50 | pid: 6359][log message]
+ */
+void write_to_log(int level, const char *file, int line, const char *message, ...);
+
+#define LOG(level, args...)    write_to_log(level, __FILE__, __LINE__, args)
+
+#endif 
