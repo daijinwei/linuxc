10a942c9335f10ceb61d0fe33e24b9d0c86e5d85
daijinwei <daijinwei41@gmail.com>
2015-09-20 18:28:38 -0700

    get addr from gethostbyname

--- unp/C11/names/hostent.c
+++ unp/C11/names/hostent.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <netdb.h>
 
 #define BUF_SIZE	1024
 #define LISTEN_BACKLOG	50
@@ -20,8 +17,32 @@
 
 int main(int argc, char *argv[])
 {
+    char *hostname = NULL, *addr = NULL;
+    char **pptr = NULL;
+    char str[INET_ADDRSTRLEN];
     if (argc < 2){
-        printf("Usage: ./hostent <host>"); 
+        printf("Usage: ./hostent <host>\n"); 
         exit(EXIT_FAILURE);
+    }
+    hostname = argv[1];
+    struct hostent *hptr = NULL;
+    if ( NULL == (hptr = gethostbyname(hostname))) {
+        handle_error("gethostbyname failed\n");
+    }
+    printf("official name: %s\n", hptr->h_name);
+
+    for (pptr = hptr->h_aliases; NULL != *pptr; pptr++) {
+        printf("aliases name: %s\n", *pptr);
+    }
+
+    switch (hptr->h_addrtype) {
+        case AF_INET: 
+                pptr = hptr->h_addr_list;
+                for ( ; NULL != *pptr; pptr++){
+                    addr = inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str));
+                }
+            break;
+        default: 
+            break;
     }
 }
